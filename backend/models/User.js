573ef4ce80718cc008356b320f6b7d1bcb5cdde9
akashdhote22 <akashdhote2525@gmail.com>
2026-09-18 12:32:43 +0530

    Fix user model filename

--- backend/models/User.js
+++ backend/models/User.js
@@ -33,4 +33,4 @@ const userSchema = new mongoose.Schema(
 );
 
 module.exports =
-  mongoose.models.User || mongoose.model("User", userSchema);
+  mongoose.models.user || mongoose.model("user", userSchema);
